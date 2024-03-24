0688e58a039fe8e36e4a885e41efe1f82814d77d
Dalha <dalhaldalha@gmail.com>
2024-03-24 22:44:46 +0100

    10:44pm 3/24/2024

--- main.cpp
+++ main.cpp
@@ -2,8 +2,23 @@
 using namespace std;
 
 int main() {
-  cout << "Welcome to the Bank of Criminals!! \nHow would you like to Proceed....\n";
+  int any;
+  char newAccount = 'N' || 'n';
+  char oldAccount = 'A';
+  cout << "Welcome to the Bank of Criminals!! \nPress any key to continue....\n";
+  cout << "If you want to open a new accout type (N) or if you already have an account with us, type (A) \n";
+  cin >> oldAccount;
+  if (oldAccount) {
+    cout << "Welcome back to your account \n";
+    cout << "How would you like to proceed? \n";
+    cin >> any;
+    cout << "1.Check Balance\n2.Deposit\n3.Withdraw\n4.Exit Bank";
 
+  } else  {
+    cout << "Welcome new user \n";
+    
+  }
+  
   
   return 0;
 }
